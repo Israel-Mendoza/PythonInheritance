81506e7bb739373458848891970c3e3a1e175931
Israel Mendoza <xander_irsl@hotmail.com>
2020-08-28 00:15:27 -0500

    All classes inherit from the object class

--- Inheritance1.py
+++ Inheritance1.py
@@ -33,5 +33,6 @@ square = Square()
 
 # Testing subclasses
 print(f"Is Ellipse a subclass of Shape? {issubclass(Shape, Square)}")
+print(f"Is Ellipse a subclass of object? {issubclass(Shape, object)}")
 print(f"Is circle an instance of the class of Shape? {isinstance(circle, type(shape))}")
 print(f"Is Shape a subclass of Shape? {issubclass(Shape, Shape)}!!!")
